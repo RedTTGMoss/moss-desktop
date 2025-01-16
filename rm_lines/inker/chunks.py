cbe8e05c84ba2512d7a45c6cba847a8bc98bd6da
JustRedTTG <redtonehair@gmail.com>
2025-01-16 09:32:04 +0200

    add pnzc

--- rm_lines/inker/chunks.py
+++ rm_lines/inker/chunks.py
@@ -1,7 +1,13 @@
+import pygameextra as pe
 from rmscene import SceneTree
 
 
 class rM_Chunks:
     def __init__(self, tree: SceneTree):
         self.tree = tree
-        # self.
+        self.pnzc = pe.PanAndZoomChunks(
+            self.get
+        )
+
+    def get(self, x, y):
+        pass
