dec65f36cf5fe1f2b9ab715c2d880f1919e1d3ce
JustRedTTG <redtonehair@gmail.com>
2025-03-20 17:20:54 +0200

    Rename context_menu.py to settings_menu.py

--- gui/screens/settings/__init__.py
+++ gui/screens/settings/__init__.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING, Dict
 import pygameextra as pe
 
 from gui import APP_NAME
-from .context_menu import SettingsSidebarChain, BackButton, parse_menu_xml
+from .settings_menu import SettingsSidebarChain, BackButton, parse_menu_xml
 from .settings_view import SettingsView
 from ...defaults import Defaults
 from ...events import ResizeEvent
