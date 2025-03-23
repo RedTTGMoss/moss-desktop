af833dde04314c35eb5b2aea46e52eff4c1d483f
red <red@redttg.com>
2025-03-23 13:40:06 +0200

    Add file dialog tester

---
+++ test_file_dialog.py
@@ -0,0 +1,28 @@
+import json
+from pprint import pprint
+from pygameextra import settings
+from box import Box
+
+with open('config.json', 'r') as f:
+    config = Box(json.load(f))
+
+class gc:
+    config = config
+
+class doc:
+    class metadata:
+        visible_name = 'test'
+
+setattr(settings, 'game_context', gc)
+setattr(settings, 'config_file_path', 'config.json')
+setattr(settings, 'config', config)
+
+from gui import file_prompts as fp
+
+def callback(data):
+    pprint(data)
+
+fp.import_prompt(callback)
+fp.import_debug(callback)
+fp.notebook_prompt(callback)
+fp.export_prompt(doc, callback)
