facf928f3ac7c473fb8f9ffc69ac51c927fc739a
RedTTG <red@redttg.com>
2026-03-25 03:19:22 +0200

    Improve transfer tool upload progress

--- tools/transfer_tool.py
+++ tools/transfer_tool.py
@@ -5,7 +5,8 @@ import threading
 import time
 
 import humanize
-from rm_api import API, FileSyncProgress, DEFAULT_REMARKABLE_URI, DEFAULT_REMARKABLE_DISCOVERY_URI, Document
+from rm_api import API, FileSyncProgress, DEFAULT_REMARKABLE_URI, DEFAULT_REMARKABLE_DISCOVERY_URI, \
+    DocumentSyncProgress, Document
 from slashr import SlashR
 
 from gui.sync_stages import SYNC_STAGE_TEXTS
@@ -32,17 +33,31 @@ def get_download_info(api_used):
     return ""
 
 
+document_sync_progress = []
+
+
 def keep_track(progress: FileSyncProgress):
     with SlashR() as sr:
         while not progress.finished:
-            sr.print(
-                f"{SYNC_STAGE_TEXTS[progress.stage]} {progress.done} / {progress.total}")
+            progresses = list(filter(lambda prog: not prog.finished, document_sync_progress))
+            done = sum(map(lambda prog: prog.done, progresses))
+            total = sum(map(lambda prog: prog.total, progresses))
+            if done < total:
+                sr.print(
+                    f"{SYNC_STAGE_TEXTS[progress.stage]} {progress.done} / {progress.total} - "
+                    f"{humanize.naturalsize(done)} / {humanize.naturalsize(total)}"
+                )
+            else:
+                sr.print(
+                    f"{SYNC_STAGE_TEXTS[progress.stage]} {progress.done} / {progress.total}")
             time.sleep(0.1)
 
 
 def hook(event):
     if isinstance(event, FileSyncProgress):
         threading.Thread(target=keep_track, args=(event,)).start()
+    elif isinstance(event, DocumentSyncProgress):
+        document_sync_progress.append(event)
 
 
 if os.path.exists(DIR):
@@ -86,10 +101,11 @@ input("Confirm?")
 
 files = list(api.documents.values()) + list(api.document_collections.values())
 
-for file in files:
-    if isinstance(file, Document):
-        if file.content.size_in_bytes >= 1e+8:
-            print(f"Document {file.metadata.visible_name} is larger than 100mb, skipping upload")
-            files.remove(file)
+if input("Limit 100mb uploads for cloudflare? [Y,n]").lower()[0] != 'n':
+    for file in files:
+        if isinstance(file, Document):
+            if file.content.size_in_bytes >= 1e+8:
+                print(f"Document {file.metadata.visible_name} is larger than 100mb, skipping upload")
+                files.remove(file)
 
 api2.upload_many_documents(files, unload=True)
